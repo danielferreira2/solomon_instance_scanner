b7a24b301fbe60c0a3f71a354ee4699cc5d6cfa6
Daniel de Faria Ferreira <danieldfariaferreira@gmail.com>
2025-06-05 00:13:05 -0300

    .

--- main.cpp
+++ main.cpp
@@ -1,37 +1,36 @@
 #include "src/solomon_scanner.h"
 #include <cmath>
 
-//===========================temp======================================
-auto euclidean_distance = [](const customer& a, const customer& b) -> double {
-    int dx = a.x - b.x;
-    int dy = a.y - b.y;
-    return std::sqrt(dx*dx + dy*dy);
-};
+namespace dists {
+    auto euclidean_distance = [](const customer& a, const customer& b) -> double {
+        int dx = a.x - b.x;
+        int dy = a.y - b.y;
+        return std::sqrt(dx*dx + dy*dy);
+    };
 
-auto weighted_distance = [](const customer& a, const customer& b) -> double {
-    int dx = a.x - b.x;
-    int dy = a.y - b.y;
-    double distance = std::sqrt(dx * dx + dy * dy);
-    
-    double weight_service_time = 0.5;
-    return distance + weight_service_time * std::abs(a.service_time - b.service_time);
-};
+    auto weighted_distance = [](const customer& a, const customer& b) -> double {
+        int dx = a.x - b.x;
+        int dy = a.y - b.y;
+        double distance = std::sqrt(dx * dx + dy * dy);
+        
+        double weight_service_time = 0.5;
+        return distance + weight_service_time * std::abs(a.service_time - b.service_time);
+    };
 
-auto rich_weighted_distance = [](const customer& a, const customer& b) -> double {
-    int dx = a.x - b.x;
-    int dy = a.y - b.y;
-    double distance = std::sqrt(dx * dx + dy * dy);
+    auto rich_weighted_distance = [](const customer& a, const customer& b) -> double {
+        int dx = a.x - b.x;
+        int dy = a.y - b.y;
+        double distance = std::sqrt(dx * dx + dy * dy);
 
-    double weight_demand = 0.3;
-    double weight_window = 0.1;
+        double weight_demand = 0.3;
+        double weight_window = 0.1;
 
-    int demand_diff = std::abs(a.demand - b.demand);
-    int window_diff = std::abs(a.ready_time - b.ready_time);
+        int demand_diff = std::abs(a.demand - b.demand);
+        int window_diff = std::abs(a.ready_time - b.ready_time);
 
-    return distance + weight_demand * demand_diff + weight_window * window_diff;
-};
-
-// =================================================================
+        return distance + weight_demand * demand_diff + weight_window * window_diff;
+    };
+}
 
 int main() {
     try {
@@ -39,7 +38,7 @@ int main() {
         
         std::cout << p;
         //===============================temp========================
-        auto distance_matrix= p.adjacency_matrix(euclidean_distance);
+        auto distance_matrix= p.adjacency_matrix(dists::weighted_distance);
         for(int i=0;i<4; i++){
             for (int j=0;j<4; j++){
                 std::cout<< distance_matrix[i][j] << " "; 
